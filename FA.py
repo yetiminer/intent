0a79469a16294ebdf9ef371cc2d1bdc3551ad53b
henry ashton <github@yetiminer.com>
2021-07-08 11:11:04 +0100

    add FQ and FA files

---
+++ FA.py
@@ -0,0 +1,212 @@
+from collections import deque, namedtuple
+import numpy as np
+from random import random
+from TQ import TillQueue
+
+Chit=namedtuple('Chit',['name','entrytime'])
+Receipt=namedtuple('Receipt',['name','entrytime','exittime','status'])
+
+class FishAuction():
+    def __init__(self,departure_rate,arrival_rate,fire_rate,q_names=None):
+        assert len(departure_rate)==len(arrival_rate)
+        self.arrival_rate=arrival_rate
+        self.departure_rate=departure_rate
+        self.q_num=len(departure_rate)
+        if q_names is None: q_names=range(self.q_num)
+        self.queues={q: TillQueue(q,d,a) for q,d,a in zip(q_names,departure_rate,arrival_rate)}
+        self.customer_count=0
+        self.fire=False
+        self.fire_rate=fire_rate
+        self.customer_where={}
+        self.customer_chits={}
+        self.time=0
+    
+    @staticmethod
+    def FA_constructor(departure_rate,arrival_rate,fire_rate,queues,queue_waits):
+        FA=FishAuction(departure_rate,arrival_rate,fire_rate,queue_dic.keys())
+        
+    @property
+    def waiting_status(self):
+        return {q_num:[self.customer_chits[cust_id].entrytime for cust_id in q] for q_num,q in self.queues.items()}
+    
+    @property
+    def queue_status(self):
+        return {q_num:list(q.q) for q_num,q in self.queues.items()}
+            
+        
+    def __repr__(self):
+        return str(f'Queues {self.queues}, Records where {self.customer_where}')
+        
+    def get_chit(self,cust_id):
+        assert cust_id in self.customer_chits
+        return customer_chits[cust_id]
+        
+    def new_customer(self,customer=None):
+        #adds a new customer to the records, gives them a chit and a name if unspecified
+        if customer is None: customer=self.customer_count      
+        self.customer_count+=1   
+        chit=self.record_chit(customer)     
+        return customer,chit
+    
+    def decide_fire(self):
+        
+        fire=False
+        if random()<self.fire_rate:
+            fire=True
+        return fire
+        
+    
+    def step(self,anon=False,fire=None):
+        self.time+=1
+        
+        ##decide if market on fire  
+        if fire is None: fire=self.decide_fire()        
+        ##get the new arrivals to the market queues
+        chits=self._process_arrivals()        
+        ##get the new departures
+        receipts=self._process_departures(fire)        
+        ##format the state information
+        if anon:
+            state= {'entries':{q:len(ch) for q,ch in chits.items()},
+                    'exits':{q:len(rec) for q,rec in receipts.items()},
+                   'queues':{q:de.len for q,de in self.queues.items()},
+                    'q_waits':self.waiting_status,
+                   'fire':fire} 
+        else:
+            state= {'entries':chits, 'exits':receipts,
+                    'queues':self.queue_status,'q_waits':self.waiting_status,
+                    'fire':fire}
+        
+        return state
+        
+              
+    def _process_departures(self,fire=False):    
+        if fire:
+            #exit without paying
+            #exit_list=[c for v, q in self.queues.items() for c in q.purge()]
+            exit_list={v:q.purge() for v,q in self.queues.items()}
+            status='EWOP'           
+        else:
+            #exit with paying
+            #exit_list=[c for v, q in self.queues.items() for c in q.serve()]
+            exit_list={v:q.serve() for v,q in self.queues.items()}
+            
+            status='Paid'
+            
+        #returns the exit reciepts and updates the internal records
+        #return [self.exit_queue(c,status) for c in exit_list]
+        return {q:[self.exit_queue(cust_id,status) for cust_id in cust_ids] for q,cust_ids in exit_list.items()}
+    
+    def _process_arrivals(self):
+        chit_d={}
+        
+        #get the number of arrivals for each queue
+        arrivals=np.random.poisson(self.arrival_rate)
+        
+        #iterate through each queue and associated arrival to add customers
+        for q_num,q_arrivals in zip(range(self.q_num),arrivals):
+            for i in range(q_arrivals):
+                chit_d[q_num]=self.new_arrival(q_num,cust_id=None)
+        return chit_d 
+        #[c for _,cl in chit_d.items() for c in cl]    
+            
+    def init_queues(self,r):
+        if type(r)==int:
+            for v in self.queues:
+                for i in range(r):
+                    cust_id,chit=self.new_customer()
+                    self.enter_queue(v,cust_id)
+        elif type(r)==dict:
+            for v,chit_list in r.items():
+                for chit in chit_list:
+                    self.enter_queue(v,)
+            
+                
+                
+    def empty_queues(self,status='test'):
+        #empties all queues and returns the list of chits
+        exit_list=[]
+        for v,q in self.queues.items():
+            exit_list.append(q.purge())
+            
+        return [self.exit_queue(cust_id,status) for cust_id in exit_list] 
+    
+    def enter_queue(self,q_num,cust_id): 
+        
+        assert cust_id in self.customer_chits
+        try:
+            #add the customer to a queue
+            self.queues[q_num].add(cust_id)
+            #record which queue the customer is in
+            self.record_where(cust_id,q_num)
+        except KeyError:
+            print(f'No such Q num {q_num}')
+            
+    def record_where(self,cust_id,queue):
+        assert cust_id not in self.customer_where
+        self.customer_where[cust_id]=queue
+        
+    def record_chit(self,cust_id):
+        assert cust_id not in self.customer_chits
+        chit=Chit(cust_id,self.time)
+        self.customer_chits[cust_id]=chit
+        return chit
+    
+    def exit_queue(self,cust_id,status):
+        self._record_queue_exit(cust_id)
+        assert status in ['EWOP','Paid','test']
+        rec=self._exit_chit(cust_id,status)
+        return rec
+    
+    
+    def _record_queue_exit(self,cust_id):
+        self.customer_where.pop(cust_id)
+    
+    def _exit_chit(self,cust_id,status):
+        assert status in ['EWOP','Paid','test']
+        chit=self.customer_chits.pop(cust_id)
+        rec= Receipt(cust_id,chit.entrytime,self.time,status)
+        return rec
+    
+    def swap_queue(self,cust_id,new_queue):
+        #find the queue
+        q_num=self.customer_where[cust_id]
+        
+        #exit the queue
+        self.queues[q_num].exit_queue(cust_id)
+        
+        #record queue exit 
+        self._record_queue_exit(cust_id)
+        
+        #enter the new queue (and record new position)
+        self.enter_queue(new_queue,cust_id)
+        
+    def new_arrival(self,q_num,cust_id=None):
+        #process a new arrival
+        assert q_num in self.queues
+        #issue a chit
+        cust_id,chit=self.new_customer(cust_id)
+        #enter a queue (with all the record keeping)
+        self.enter_queue(q_num,cust_id)
+        return chit  
+
+    def where_is(self,cust_id):
+        #find the queue
+        q_num=self.customer_where[cust_id]
+        
+        #look for it in the queue
+        try:          
+            return q_num,self.queues[q_num].where_is(cust_id)
+        except IndexError:
+            raise
+            
+class FA_tests():
+    def __init__(self,FA):
+        self.FA=FA
+    
+    def _check_records(self):
+        for cid in self.FA.customer_where:
+            assert cid in self.FA.customer_chits
+        for cid in self.FA.customer_chits:
+            assert cid in self.FA.customer_where  
+        return True
---
+++ TQ.py
@@ -0,0 +1,63 @@
+from collections import deque, namedtuple
+import numpy as np
+from random import random
+
+class TillQueue():
+    def __init__(self,name,departure_rate,arrival_rate):
+        self.name=name
+        self.q=deque()
+        self.departure_rate=departure_rate
+        self.arrival_rate=arrival_rate #for reference
+        
+    def __repr__(self):
+        return str(self.q)
+    
+    @property
+    def len(self):
+        return len(self.q)
+        
+    def add(self,person):
+        self.q.append(person)
+        
+    def serve(self,num=None):
+        x=[]
+        if num is None: num=np.random.poisson(self.departure_rate)
+        if num>0:
+            for i in range(num):
+                try:
+                    x+=[self.q.popleft()]
+                except IndexError:
+                    x+=[]
+        return x
+        
+    def purge(self):
+        exit_list=list(self.q)
+        self.q.clear()
+        return exit_list
+    
+    def exit_queue(self,person):
+        self.q.remove(person)
+        
+    def where_is(self,cust_id):
+        for idx,cid in enumerate(self.q):
+            if cid==cust_id:
+                return idx
+        
+        return IndexError
+    
+    def __iter__(self):
+        return TillQueueIterator(self.q)
+    
+class TillQueueIterator():
+    def __init__(self, data_sequence):
+       self.idx = 0
+       self.data = data_sequence
+    def __iter__(self):
+       return self
+    def __next__(self):
+       self.idx += 1
+       try:
+           return self.data[self.idx-1]
+       except IndexError:
+           self.idx = 0
+           raise StopIteration  # Done iterating.
